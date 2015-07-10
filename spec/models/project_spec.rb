1c239e27d879a74214001b05ae0ceaee3cc996b4
Axel <kurtastraube@gmail.com>
2015-07-10 08:56:53 -0700

    Add and pass project/skill association spec.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Project do
   it { should validate_presence_of :name }
+  it ("should belong to a skill") do
+    skill = Skill.create({:name => "JavaScript"})
+    project = Project.create({:name => "Pig Dice"})
+    skill.projects.push(project)
+    expect(skill.projects()).to(eq([project]))
+  end
 end
